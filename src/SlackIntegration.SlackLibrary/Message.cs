a3aff127798d6a3eaf567df9cfa52c82396b6aa2
radu-matei <radu.matei@equilobe.com>
2016-02-16 01:10:30 +0200

    Added Slack message model

---
+++ src/SlackIntegration.SlackLibrary/Message.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace SlackIntegration.SlackLibrary
+{
+    public class Message
+    {
+        [JsonProperty("channel")]
+        public string Channel { get; set; }
+
+        [JsonProperty("username")]
+        public string UserName { get; set; }
+
+        [JsonProperty("text")]
+        public string Text { get; set; }
+    }
+}
