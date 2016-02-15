028b7e517f51d0046dd3cf34741532260622af7e
radu-matei <radu.matei@equilobe.com>
2016-02-16 01:13:41 +0200

    Added controller to publish message to Slack channel

---
+++ src/SlackIntegration.API/Controllers/SlackController.cs
@@ -0,0 +1,17 @@
+﻿using SlackIntegration.SlackLibrary;
+using System.Configuration;
+using System.Web.Http;
+
+namespace SlackIntegration.Controllers
+{
+    public class SlackController : ApiController
+    {
+        private SlackClient SlackClient = new SlackClient(ConfigurationManager.AppSettings["SlackWebHookUri"]);
+
+        [HttpPost]
+        public void PostMessage(Message message)
+        {
+            SlackClient.PostMessage(message);
+        }
+    }
+}
