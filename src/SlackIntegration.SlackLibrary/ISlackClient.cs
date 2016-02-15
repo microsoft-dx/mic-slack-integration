e7718d992de4fc9de2cdfa836e8de28bf738a584
radu-matei <radu.matei@equilobe.com>
2016-02-16 01:11:28 +0200

    Added client to publish messages to the specific Slack  channel

---
+++ src/SlackIntegration.SlackLibrary/ISlackClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SlackIntegration.SlackLibrary
+{
+    public interface ISlackClient
+    {
+        Uri Uri { get; }
+
+        void PostMessage(string text, string userName = null, string channel = null);
+        void PostMessage(Message message);
+    }
+}
---
+++ src/SlackIntegration.SlackLibrary/SlackClient.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Specialized;
+using System.Net;
+
+namespace SlackIntegration.SlackLibrary
+{
+    public class SlackClient : ISlackClient
+    {
+        public Uri Uri { get; private set; }
+
+        public SlackClient(string uriString)
+        {
+            Uri = new Uri(uriString);
+        }
+
+        public void PostMessage(string text, string userName = null, string channel = null)
+        {
+            Message message = new Message()
+            {
+                Channel = channel,
+                UserName = userName,
+                Text = text
+            };
+
+            PostMessage(message);
+        }
+
+        public void PostMessage(Message message)
+        {
+            string jsonMessage = JsonConvert.SerializeObject(message);
+
+            using(WebClient webClient = new WebClient())
+            {
+                NameValueCollection data = new NameValueCollection();
+                data.Add("payload", jsonMessage);
+
+                var response = webClient.UploadValues(Uri, "POST", data);
+
+                //TODO - do something with the responseText
+                //string responseText = webClient.Encoding.GetString(response);
+            }
+        }
+
+    }
+}
