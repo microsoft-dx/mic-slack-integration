f08ad64866e66c9f9347fc89b50dd3ab78ea7e6a
radu-matei <radu.matei@equilobe.com>
2016-02-16 20:55:16 +0200

    Created Startup  configuration for Autofac - moved global configuration to Owin integration of Autofac - configured WebApi - configured SignalR

--- src/SlackIntegration.API/Startup.cs
+++ src/SlackIntegration.API/Startup.cs
@@ -1,5 +1,12 @@
-﻿using Microsoft.Owin;
+﻿using Autofac;
+using Autofac.Integration.SignalR;
+using Autofac.Integration.WebApi;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Infrastructure;
+using Microsoft.Owin;
 using Owin;
+using SlackIntegration.Utils;
+using System.Web.Http;
 
 [assembly: OwinStartup(typeof(SlackIntegration.Startup))]
 
@@ -9,7 +16,44 @@ namespace SlackIntegration
     {
         public void Configuration(IAppBuilder app)
         {
-            app.MapSignalR();
+            var config = new HttpConfiguration();
+            WebApiConfiguration(config);
+
+            var container = new AutofacContainer().Container;
+
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+
+            app.UseAutofacMiddleware(container);
+            app.UseAutofacWebApi(config);
+            app.UseWebApi(config);
+
+            var resolver = new AutofacDependencyResolver(container);
+            app.MapSignalR(new HubConfiguration
+            {
+                Resolver = resolver,
+                EnableDetailedErrors = true
+            });
+
+            ConfigureSignalR(container, resolver);
+        }
+
+        private void ConfigureSignalR(IContainer container, IDependencyResolver resolver)
+        {
+            var newBuilder = new ContainerBuilder();
+            newBuilder.RegisterInstance(resolver.Resolve<IConnectionManager>());
+
+            newBuilder.Update(container);
+        }
+
+        private void WebApiConfiguration(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
         }
     }
 }
