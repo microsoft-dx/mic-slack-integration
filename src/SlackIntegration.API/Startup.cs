3a8efb3ba8386beedafc12b29f54433e3821ebbf
radu-matei <radu.matei@equilobe.com>
2016-02-16 13:00:23 +0200

    Added SignalR to project

---
+++ src/SlackIntegration.API/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SlackIntegration.Startup))]
+
+namespace SlackIntegration
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
