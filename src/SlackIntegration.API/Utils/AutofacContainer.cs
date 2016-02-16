d09249f6f0bdb1c816be2a232c2733b88322e645
radu-matei <radu.matei@equilobe.com>
2016-02-16 20:54:12 +0200

    Added Autofac Container - registered Api controllers - registered SignalR hubs - registered SlackClient as ISlackClient

---
+++ src/SlackIntegration.API/Utils/AutofacContainer.cs
@@ -0,0 +1,33 @@
+﻿using Autofac;
+using Autofac.Integration.SignalR;
+using Autofac.Integration.WebApi;
+using SlackIntegration.SlackLibrary;
+using System.Configuration;
+using System.Reflection;
+
+namespace SlackIntegration.Utils
+{
+    public class AutofacContainer
+    {
+        public IContainer Container { get; set; }
+
+        public AutofacContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly())
+                .PropertiesAutowired()
+                .InstancePerRequest();
+
+            builder.RegisterType<SlackClient>()
+                .As<ISlackClient>()
+                .PropertiesAutowired()
+                .WithParameter("uriString", ConfigurationManager.AppSettings["SlackWebHookUri"]);
+
+            builder.RegisterHubs(Assembly.GetExecutingAssembly())
+                .PropertiesAutowired();
+
+            Container = builder.Build();
+        }
+    }
+}
