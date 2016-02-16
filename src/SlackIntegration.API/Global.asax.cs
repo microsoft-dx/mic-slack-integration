0fb4e5259de0000e8cf4593f953a7e02389cf225
radu-matei <radu.matei@equilobe.com>
2016-02-16 20:53:18 +0200

    Added Dependency Injection - added Autofac packages - removed global  configuration for WebApi

--- src/SlackIntegration.API/Global.asax.cs
+++ src/SlackIntegration.API/Global.asax.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
-using System.Web.Routing;
-
-namespace SlackIntegration
+﻿namespace SlackIntegration
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
-            GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
 }
