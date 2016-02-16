0858c0369975a901b6bfc3adfac5da1da902aeb3
radu-matei <radu.matei@equilobe.com>
2016-02-16 12:59:37 +0200

    Added Slack Command for deserialization

---
+++ src/SlackIntegration.SlackLibrary/SlackCommand.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+
+namespace SlackIntegration.SlackLibrary
+{
+    public class SlackCommand
+    {
+        
+        [JsonProperty(PropertyName = "token")]
+        public string Token { get;  set; }
+
+        [JsonProperty(PropertyName = "team_id")]
+        public string TeamId { get;  set; }
+
+        [JsonProperty(PropertyName = "team_domain")]
+        public string TeamDomain { get; set; }
+
+        [JsonProperty(PropertyName = "channel_id")]
+        public string ChannelId { get;  set; }
+
+        [JsonProperty(PropertyName = "channel_name")]
+        public string ChannelName { get;  set; }
+
+        [JsonProperty(PropertyName = "user_id")]
+        public string UserId { get;  set; }
+
+        [JsonProperty(PropertyName = "user_name")]
+        public string UserName { get;  set; }
+
+        [JsonProperty(PropertyName = "command")]
+        public string Command { get;  set; }
+
+        [JsonProperty(PropertyName = "text")]
+        public string Text { get;  set; }
+
+        [JsonProperty(PropertyName = "response_url")]
+        public string ResponseUrl { get;  set; }
+
+    }
+}
