24152b74b0a98dfd620c14de1e58bf7869a67a46
radu-matei <radu.matei@equilobe.com>
2016-02-18 19:58:13 +0200

    Added EF database context

---
+++ src/SlackIntegration.API/DAL/SlackDbContext.cs
@@ -0,0 +1,14 @@
+﻿using SlackIntegration.SlackLibrary;
+using System.Data.Entity;
+
+namespace SlackIntegration.DAL
+{
+    public class SlackDbContext : DbContext
+    {
+        public SlackDbContext(string connectionString): base(connectionString)
+        {
+        }
+
+        public DbSet<SlackMessage> Messages { get; set; }
+    }
+}
